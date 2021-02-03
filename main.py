80803dd97402f95984194dee87174614d83a14ac
shinhwagk <shanghai_sl@hotmail.com>
2021-02-03 14:06:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ hds = [{'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.
 
 
 def book_spider(book_tag):
-    page_num = 49
+    page_num = 0
     book_list = []
 
     while(True):
