10948f8f3ad6a51a8b1a3f85b4e291d77759768c
shinhwagk <shanghai_sl@hotmail.com>
2021-02-03 14:19:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     # book_tag_lists = ['科普','经典','生活','心灵','文学']
     # book_tag_lists = ['科幻','思维','金融']
     # book_tag_lists = ['个人管理', '时间管理', '投资', '文化', '宗教']
-    book_lists = do_spider(book_tag_lists)
-    # print(book_lists)
-    print_book_lists_excel(book_lists, book_tag_lists)
+    for tag in book_tag_lists:
+        book_lists = do_spider([tag])
+        print_book_lists_excel(book_lists, book_tag_lists)
     # print(get_book_info('https://book.douban.com/subject/1449351/'))
